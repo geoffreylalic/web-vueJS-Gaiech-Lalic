11e1887dad53489805237a14bfb23333e4265e9c
Wajjdi <wajdi.gaiech@etu.unice.fr>
2021-10-05 12:08:02 +0200

    Création du menu

--- Projet TechnoWeb/client/src/components/Menu.vue
+++ Projet TechnoWeb/client/src/components/Menu.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        Menu component
+        <md-tabs class="md-primary" md-alignment="centered">
+      <md-tab id="tab-home" md-label="Home"></md-tab>
+      <md-tab id="tab-pages" md-label="Pages"></md-tab>
+      <md-tab id="tab-posts" md-label="Posts"></md-tab>
+      <md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+    </md-tabs>
     </div>
 </template>
 
