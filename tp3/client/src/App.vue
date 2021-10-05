404b0277b66cea6ad2c55acfcbf46b2cdb9e6fc9
Wajjdi <wajdi.gaiech@etu.unice.fr>
2021-10-05 11:06:07 +0200

    init

---
+++ tp3/client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <ListeRestaurant/>
+  </div>
+</template>
+
+<script>
+
+import ListeRestaurant from './components/ListeRestaurant.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    ListeRestaurant
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
