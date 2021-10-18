ed0d6aee6e5935f147f598155b825d827fe328a1
Geoffrey <geoffrey0151@gmail.com>
2021-10-18 15:10:36 +0200

    Ajout des du composant de recherche dans la page d'accueil

--- Projet TechnoWeb/client-vuetify/src/components/Accueil.vue
+++ Projet TechnoWeb/client-vuetify/src/components/Accueil.vue
@@ -2,23 +2,20 @@
   <v-app>
     <Menu />
     <Header />
-    <Recherche />
     <ListeRestaurant />
   </v-app>
 </template>
 
 <script>
-import Recherche from './components/Recherche.vue'
 import Header from "./components/Header.vue";
 import Menu from "./components/Menu.vue";
 import ListeRestaurant from "./components/ListeRestaurant.vue";
 
 export default {
-  name: "App",
+  name: "Accueil",
 
   components: {
     Header,
-    Recherche,
     Menu,
     ListeRestaurant
   },
---
+++ Projet TechnoWeb/client-vuetify/src/components/Recherche.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="5">
+        <v-text-field :rules="rules" label="#Restaurant"></v-text-field>
+      </v-col>
+      <v-col cols="5">
+        <v-text-field :rules="rules" label="#Page Size"></v-text-field>
+      </v-col>
+      <v-col cols="2">
+        <v-btn icon color="green">
+          <v-icon>mdi-cached</v-icon>
+        </v-btn></v-col
+      >
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Recherche",
+  data: () => ({}),
+};
+</script>
