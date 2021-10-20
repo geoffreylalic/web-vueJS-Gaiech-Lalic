445971a9a7480e21725d8fbca934831747fbd073
Wajjdi <wajdi.gaiech@etu.unice.fr>
2021-10-20 20:22:43 +0200

    Initialisation du menu

--- Projet TechnoWeb/client-vuetify/src/components/Menu.vue
+++ Projet TechnoWeb/client-vuetify/src/components/Menu.vue
@@ -1,16 +1,22 @@
 <template>
-    <div>
-        <H1>YO</H1>
-    </div>
+  <v-tabs id="menu" fixed-tabs background-color="indigo" dark>
+    <v-tab> Accueil </v-tab>
+    <v-tab> Localisation </v-tab>
+    <v-tab> Admin </v-tab>
+  </v-tabs>
 </template>
 
 <script>
-
 export default {
-    name:'Menu',
-    data : ()=>({
-    }),
-    methods: {},
-    mounted() {}
-}
+  name: "Menu",
+  data: () => ({}),
+  methods: {},
+  mounted() {},
+};
 </script>
+
+<style>
+#menu {
+  flex: none;
+}
+</style>
